a1ac0ad60db25931517ec6a12cad8dcf20ac4a2a
renmaeren <maegan.renick@allen.kyschools.us>
2026-08-01 17:11:48 -0500

    Add Google Apps Script configuration

--- config/google-config.js
+++ config/google-config.js
@@ -2,3 +2,11 @@ window.PATRIOT_GOOGLE_CONFIG = {
   CLIENT_ID: "390057015202-jrl17ud8lp1rt6278ks1bp3v373049lc.apps.googleusercontent.com",
   API_KEY: "AIzaSyD0ZpeOMV8Z4wKoWrXS20GYO8sHGcmbhtM"
 };
+/*
+==========================================
+Google Apps Script
+==========================================
+*/
+
+window.GOOGLE_SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycbzGckJAit70HvekLOlIwNmaPVTv5-vb8o_orjRZDK0koTW-LTT4E6bgL1J9qiHBp_41/exec";
