f9a20e7161ccad1e6751a4fb4a229f7ea30c22e0
renmaeren <maegan.renick@allen.kyschools.us>
2026-08-01 17:40:20 -0500

    Initialize Google authentication module

--- js/google-auth.js
+++ js/google-auth.js
@@ -1,1 +1,16 @@
+/*
+==========================================
+Patriot Command
+Google Authentication
+Version 1
+==========================================
+*/
 
+window.PATRIOT_AUTH = {
+    signedIn: false,
+    user: null
+};
+
+console.log(
+    "Patriot Google Auth v1 loaded."
+);
